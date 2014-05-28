96b225495f360d0731a7fab74e2a4b6ef69475f0
Mojdeh Tarighat <mojdehtarighat@gmail.com>
2014-05-27 20:28:51 -0700

    Added solution to problem 7

---
+++ questions7.py
@@ -0,0 +1,31 @@
+__author__ = 'mojdehtarighat'
+"""By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+What is the 10 001st prime number?"""
+
+def is_prime(x):
+    n = 2
+    if x<2:
+        return False
+    elif x==2:
+        return True
+    else:
+        while n<=x-1:
+            if x%n==0:
+                return False
+            else:
+                n=n+1
+    return True
+
+def xth_prime(x):
+    prime = 2
+    count = 1
+    iter = 3
+    while count < x:
+        if is_prime(iter):
+            prime = iter
+            count =count+ 1
+        iter =iter+ 2
+    return prime
+
+print xth_prime(10001)
